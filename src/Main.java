af5be3cc48d618ae33582fb57c577bf299bea41f
sara berg <samangani_sara@hotmail.com>
2017-11-13 13:19:20 +0100

    i am finish

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Random random = new Random();
+		int value = random.nextInt(21);
+		
+		if (value %2 ==0) {
+			System.out.println("even");
+		}else {
+			System.out.println("odd");
+		}
 		
 
 	}
